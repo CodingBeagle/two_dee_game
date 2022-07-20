646874dff405f447dd52a07f7849e875c2ec286e
CodingBeagle <kasper.rie@gmail.com>
2022-07-20 22:47:11 +0200

    removed 'target' dir

--- target/debug/deps/two_dee_game-1bdcbbe8b695401f.d
+++
@@ -1,5 +0,0 @@
-d:\personal-projects\two_dee_game\target\debug\deps\two_dee_game-1bdcbbe8b695401f.rmeta: src\main.rs
-
-d:\personal-projects\two_dee_game\target\debug\deps\two_dee_game-1bdcbbe8b695401f.d: src\main.rs
-
-src\main.rs:
--- target/debug/deps/two_dee_game.d
+++
@@ -1,5 +0,0 @@
-d:\personal-projects\two_dee_game\target\debug\deps\two_dee_game.exe: src\main.rs
-
-d:\personal-projects\two_dee_game\target\debug\deps\two_dee_game.d: src\main.rs
-
-src\main.rs:
