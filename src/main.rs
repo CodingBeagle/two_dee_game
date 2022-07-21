ffc0b7035d2e78af5fd3c8883ffd33e676c2ab9a
CodingBeagle <kasper.rie@gmail.com>
2022-07-21 21:43:24 +0200

    added bindgen, and made bindings for GLFW

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
 fn main() {
     println!("Hello, world!");
+
+    unsafe {
+        let glfw_init_result = glfwInit();
+        println!("{}", glfw_init_result);
+    }
 }
